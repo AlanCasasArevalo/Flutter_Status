5cdff2c1b657a03b2dda0584dff02c50ab8dfe56
Alan Casas Arevalo <alan.casas.arevalo@everis.com>
2021-01-05 08:31:17 +0100

    first commit

---
+++ lib/pages/my_app.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Flutter Demo',
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Pon_aqui_tu_titulo'),
+        ),
+        body: Center(
+          child: Container(
+              child: Text('Pon_aqui_tu_titulo')
+          ),
+        ),
+      ),
+    );
+  }
+}

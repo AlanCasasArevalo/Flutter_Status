f506e6f768c1f918789377111efceb0b45cd936b
Alan Casas Arevalo <alan.casas.arevalo@everis.com>
2021-01-05 15:49:11 +0100

    [FEATURE][CUBIT] * Cubit was added to app and initialized

--- lib/pages/my_app.dart
+++ lib/pages/my_app.dart
@@ -1,13 +1,20 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:status/bloc/user/user_cubit.dart';
 import 'package:status/routes/routes.dart';
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      onGenerateRoute: getDefaultRouteApplications(),
-      routes: getApplications(),
+    return MultiBlocProvider(
+      providers: [
+        BlocProvider(create: (_) => new UserCubit())
+      ],
+      child: MaterialApp(
+        debugShowCheckedModeBanner: false,
+        onGenerateRoute: getDefaultRouteApplications(),
+        routes: getApplications(),
+      ),
     );
   }
 }
--- lib/pages/page_one.dart
+++ lib/pages/page_one.dart
@@ -1,18 +1,29 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:status/bloc/user/user_cubit.dart';
 import 'package:status/common/constants.dart';
 
 class PageOne extends StatelessWidget {
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text('Pagina 1'),
       ),
-      body: UserInformation(),
+      body: BlocBuilder<UserCubit, UserState>(
+        builder: (BuildContext context, state) {
+          print(state);
+          if (state is UserInitialState) {
+            return Center(child: Text('No hay informacion del usuario'),);
+          } else {
+            return UserInformation();
+          }
+        },
+      ),
       floatingActionButton: FloatingActionButton(
         child: Icon(Icons.accessibility_new),
-        onPressed: () => Navigator.pushNamed(context, Constants.pageTwoRouteName),
+        onPressed: () =>
+            Navigator.pushNamed(context, Constants.pageTwoRouteName),
       ),
     );
   }
@@ -28,15 +39,31 @@ class UserInformation extends StatelessWidget {
       child: Column(
         crossAxisAlignment: CrossAxisAlignment.start,
         children: [
-          Text('General', style: TextStyle(fontWeight: FontWeight.bold, fontSize: 18),),
+          Text(
+            'General',
+            style: TextStyle(fontWeight: FontWeight.bold, fontSize: 18),
+          ),
           Divider(),
-          ListTile(title: Text('Nombre: '),),
-          ListTile(title: Text('Edad: '),),
-          Text('Profesiones', style: TextStyle(fontWeight: FontWeight.bold, fontSize: 18),),
+          ListTile(
+            title: Text('Nombre: '),
+          ),
+          ListTile(
+            title: Text('Edad: '),
+          ),
+          Text(
+            'Profesiones',
+            style: TextStyle(fontWeight: FontWeight.bold, fontSize: 18),
+          ),
           Divider(),
-          ListTile(title: Text('Profesion 1'),),
-          ListTile(title: Text('Profesion 2'),),
-          ListTile(title: Text('Profesion 3'),),
+          ListTile(
+            title: Text('Profesion 1'),
+          ),
+          ListTile(
+            title: Text('Profesion 2'),
+          ),
+          ListTile(
+            title: Text('Profesion 3'),
+          ),
         ],
       ),
     );
